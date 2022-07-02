5dc61d14b74920a4fac6557a55d16fbb8d139e59
Ramon Ferreira da Mota Pereira <ramon_mota@live.com>
2022-07-02 18:04:55 -0300

    open screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,14 @@ import { Footer } from "./components/footer/footer";
 
 
 import "./App.scss";
-import "./assets/style/global-style.scss"
 import Routers from "./Routers";
 import { Introdution } from "./components/Introdution/Introdution";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(true)
-
-  setTimeout(() => {
-    setIsOpen(() => false)
-  }, 3100)
 
   return (
     <>
-      {isOpen && <Introdution/>}
+      <Introdution/>
       <Header/>
       <main>
         <Routers />
--- src/components/Introdution/Introdution.tsx
+++ src/components/Introdution/Introdution.tsx
@@ -6,17 +6,8 @@ export const Introdution = () => {
     
   return (
     <div className='container-introdution'>
-        <h1>
-            <span className="row">
-                <span>Do what</span>
-            </span>
-            <span className="row">
-                <span></span>
-            </span>
-            <span className="row mt">
-                <span>you <b>love</b></span>
-            </span>
-        </h1>
+        <h2>Lorem ipsum dolor</h2>
+        <h2>Praesent ultricies </h2>
     </div>
   )
 }
