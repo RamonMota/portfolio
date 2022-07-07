d341c178df2e550e41dad6933c4e6db9363e6766
Ramon Mota <ramon_mota@live.com>
2022-07-07 00:06:21 -0300

    update sebrae case

--- src/pages/cases/sebrae/case-sebrae.tsx
+++ src/pages/cases/sebrae/case-sebrae.tsx
@@ -7,13 +7,14 @@ export const CaseSebrae = () => {
     const screenSize = useSizeScreen()
     
   return (
-    <div>
-        <h1 className='title-case'>Title</h1>
-        <iframe  
-        width='800px' 
-        height='600px'
-        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FG5LP5npZCmW2gBvDFcVrec%2FE-commerce-Sebrae" 
-        ></iframe>
+    <div className='container-case'>
+        <span className='subtitle-section'>2018 - Today</span>
+        <h1 className='title-section'>E-commerce Sebrae</h1>
+        <div className='container-iframe'>
+          <iframe
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FG5LP5npZCmW2gBvDFcVrec%2FE-commerce-Sebrae%3Fnode-id%3D0%253A1" 
+          ></iframe>
+        </div>
         
     </div>
   )
