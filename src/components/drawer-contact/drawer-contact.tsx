5ec0be6f0ebe2e8108e2a20a0d36f16f8888f226
Ramon Ferreira da Mota Pereira <ramon_mota@live.com>
2022-08-05 20:29:52 -0300

    update form

--- src/components/drawer-contact/drawer-contact.tsx
+++ src/components/drawer-contact/drawer-contact.tsx
@@ -5,76 +5,68 @@ import { Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, Draw
 
 export const DrawerContact = () => {
 
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const [ size, setSize ] = useState('xs')
-    
-    return (
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [size, setSize] = useState('xs')
+
+  return (
     <>
-        <a className='btn' onClick={onOpen}>Contact</a>
-        <Drawer
-            isOpen={isOpen}
-            placement='right'
-            onClose={onClose}
-            blockScrollOnMount={false}
-            size={size}
-          >
-            <DrawerOverlay />
-            <DrawerContent className='drawer-contact'>
-              <DrawerCloseButton />
-              <DrawerHeader>
-                <label className='subtitle-section'>Ok, now</label>
-                <h2 className='title-section '>What do you want?</h2>
-              </DrawerHeader>
-    
-              <DrawerBody>
-                <Stack spacing='24px'>
-                  <Box>
-                    <FormLabel htmlFor='username'>Name</FormLabel>
-                    <Input
-                      id='username'
-                      placeholder='Please enter user name'
-                    />
-                  </Box>
-    
-                  <Box>
-                    <FormLabel htmlFor='url'>Url</FormLabel>
-                    <InputGroup>
-                      <InputLeftAddon>http://</InputLeftAddon>
-                      <Input
-                        type='url'
-                        id='url'
-                        placeholder='Please enter domain'
-                      />
-                      <InputRightAddon>.com</InputRightAddon>
-                    </InputGroup>
-                  </Box>
-    
-                  <Box>
-                    <FormLabel htmlFor='owner'>Select Owner</FormLabel>
-                    <Select id='owner' defaultValue='segun'>
-                      <option value='segun'>Segun Adebayo</option>
-                      <option value='kola'>Kola Tioluwani</option>
-                    </Select>
-                  </Box>
-    
-                  <Box>
-                    <FormLabel htmlFor='desc'>Description</FormLabel>
-                    <Textarea id='desc' />
-                  </Box>
-                </Stack>
-              </DrawerBody>
-    
-              <DrawerFooter borderTopWidth='1px'>
-                <Button variant='outline' mr={3} onClick={onClose}>
-                  Cancel
-                </Button>
-                <Button colorScheme='blue'>Submit</Button>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
-         
+      <a className='btn' onClick={onOpen}>Contact</a>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        blockScrollOnMount={false}
+        size={size}
+      >
+        <DrawerOverlay />
+        <DrawerContent className='drawer-contact'>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <label className='subtitle-section'>Ok, now</label>
+            <h2 className='title-section '>What do you want?</h2>
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Stack spacing='24px'>
+
+              <Box>
+                <label className='label-input' htmlFor='owner'>What about</label>
+                <Select id='owner' defaultValue='segun'>
+                  <option value='segun'>-</option>
+                  <option value='kola'>Kola Tioluwani</option>
+                  <option value='kola'>Kola Tioluwani</option>
+                </Select>
+              </Box>
+
+              <Box>
+                <label className='label-input' htmlFor='username'>Your name</label>
+                <Input
+                  id='username'
+                  placeholder='Please enter your name'
+                />
+              </Box>
+              <Box>
+                <label className='label-input' htmlFor='username'>Your e-mail</label>
+                <Input
+                  id='username'
+                  placeholder='Please enter your e-mail'
+                />
+              </Box>
+
+              <Box>
+                <label className='label-input' htmlFor='desc'>Tell me more</label>
+                <Textarea placeholder='What is your interest?' id='desc' />
+              </Box>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter borderTopWidth='1px'>
+            <Button colorScheme='blue'>Submit</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+
     </>
-    )
-  }
-  
-  
+  )
+}
+
