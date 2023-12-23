6272716054baa0ba993985491d51ddf8129eadd7
Ramon Mota <ramon_mota@live.com>
2023-12-23 00:34:29 -0500

    ajustes home

--- src/components/float-sidebar/index.tsx
+++ src/components/float-sidebar/index.tsx
@@ -1,12 +1,24 @@
+import { Fragment } from 'react';
+import { useSizeScreen } from '../../hooks/height-screen';
+import { SocialMedia } from '../social-media';
 import './index.scss';
 
 export const FloatSidebar = () => {
-  return (
-    <div className='content-float-sidebar'>
-        <div className='circle-icon'/>
-        <p>Cases</p>
-        <p>Last works</p>
-        <button className='btn-principal'>Let's talk</button>
-    </div>
-  )
+
+    const { widthScreen } = useSizeScreen()
+
+    return (
+        <div className='content-float-sidebar'>
+            <div className='circle-icon' />
+            <p>Cases</p>
+            <p>Last works</p>
+            {widthScreen > 599 &&
+                <Fragment>
+                    <i className='vertical-line' />
+                    <SocialMedia />
+                </Fragment>
+            }
+            <button className='btn-principal'>Let's talk</button>
+        </div>
+    )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,50 @@
+import { useRef } from 'react';
 import { SocialMedia } from '../../components/social-media';
 import './home.scss'
 
 export const Home = () => {
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const handleScroll = (e: React.WheelEvent) => {
+        if (e.deltaY !== 0 && containerRef.current) {
+            e.preventDefault();
+
+            const currentScrollLeft = containerRef.current.scrollLeft;
+            containerRef.current.scrollLeft = currentScrollLeft + e.deltaY;
+        }
+    }
+
 
     return (
         <div className='container-home' >
             <div className='content-title'>
-                <h1>RAMON MOTA</h1>
+                <h1>RAMON <b>MOTA</b></h1>
                 <div className='content-social'>
                     <SocialMedia />
                 </div>
             </div>
-            <div className='content-float-main'>
-                <div className='content-text-main'>
-                    <p className='color-principal'>DIRTYING</p>
-                    <p className='color-principal'>MY</p>
-                    <p className='color-principal'>FINGERS</p>
-                    <p>SINCE</p>
-                    <p>2015</p>
+            <div ref={containerRef}
+                onWheel={handleScroll}
+                className='content-horizontal-scroll'>
+                <div className='content-float-main'>
+                    <div className='content-text-main'>
+                        <p className='color-principal'>DIRTYING</p>
+                        <p className='color-principal'>MY</p>
+                        <p className='color-principal'>FINGERS</p>
+                        <p>SINCE</p>
+                        <p>2015</p>
+                    </div>
+                    <div className='content-skills'>
+                        <p>front end developer</p>
+                        <p>Illustrator</p>
+                        <p>UI/UX</p>
+                    </div>
                 </div>
-                <div className='content-skills'>
-                    <p>front end developer</p>
-                    <p>Illustrator</p>
-                    <p>UI/UX</p>
-                </div>
+                <div className='card-fake'/>
+                <div className='card-fake'/>
+                <div className='card-fake'/>
+                <div className='card-fake'/>
+                <div className='card-fake'/>
             </div>
         </div>
     )
