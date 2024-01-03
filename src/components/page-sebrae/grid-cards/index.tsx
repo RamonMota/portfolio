1475235ab8cba70dbc8451be8683505304c386c3
Ramon Mota <ramon_mota@live.com>
2024-01-02 21:47:49 -0500

    grid cards

---
+++ src/components/page-sebrae/grid-cards/index.tsx
@@ -0,0 +1,64 @@
+import { Tag } from '../../tag/tag'
+import { Astronaut } from '../../../pages/sebrae/svg/astronaut'
+import imgPhone from '../../../assets/img/mobile-usability-min.png'
+
+import './index.scss'
+
+export const GridCardSebrae = () => {
+    return (
+        <div className='grid-cards-sebrae'>
+            <div className='content-card large-card bg-sebrae-secondary first'>
+                <div className='content-info'>
+                    <div className='d-flex gap-xs'>
+                        <Tag name='UI/UX' />
+                        <Tag name='Front end' />
+                    </div>
+                    <div className='content-description'>
+                        <h3>Enhanced Mobile Usability</h3>
+                        <p>Implementing a mobile-first approach, we are incorporating designated breathing spaces to enhance user comfort and streamline platform accessibility.</p>
+                    </div>
+                </div>
+                <img className='top-screen' src={imgPhone} alt="screen" />
+            </div>
+            <div className='content-card shurt-card bg-sebrae-successful second'>
+                <div className='content-info'>
+                    <div className='d-flex gap-xs'>
+                        <Tag name='UI/UX' />
+                        <Tag name='Front end' />
+                    </div>
+                    <div className='content-description'>
+                        <h3>Enhanced Mobile Usability</h3>
+                        <p>Implementing a mobile-first approach, we are incorporating designated breathing spaces to enhance user comfort and streamline platform accessibility.</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='content-card shurt-card bg-sebrae-alert third'>
+                <div className='content-info'>
+                    <div className='d-flex gap-xs'>
+                        <Tag name='UI/UX' />
+                        <Tag name='Front end' />
+                    </div>
+                    <div className='content-description'>
+                        <h3>Enhanced Mobile Usability</h3>
+                        <p>Implementing a mobile-first approach, we are incorporating designated breathing spaces to enhance user comfort and streamline platform accessibility.</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='content-card large-card bg-sebrae-white fourth'>
+                <div className='content-info'>
+                    <div className='d-flex gap-xs'>
+                        <Tag name='UI/UX' />
+                        <Tag name='Front end' />
+                    </div>
+                    <div className='content-description'>
+                        <h3>Optimizing Web Performance for Visual Accessibility</h3>
+                        <p>Illustrations Enhancing User Communication: Leveraging SVG Format for Web Optimization without Compromising Performance</p>
+                    </div>
+                </div>
+                <Astronaut />
+            </div>
+        </div>
+    )
+}
--- src/pages/sebrae/index.tsx
+++ src/pages/sebrae/index.tsx
@@ -1,10 +1,9 @@
 
 import { useEffect, useState } from 'react';
 import './index.scss'
-import { Astronaut } from './svg/astronaut';
 import { useHistory } from 'react-router';
 import { Paths } from '../../config/paths/path';
-import { Tag } from '../../components/tag/tag';
+import { GridCardSebrae } from '../../components/page-sebrae/grid-cards';
 
 export const Sebrae = () => {
   const history = useHistory()
@@ -25,31 +24,19 @@ export const Sebrae = () => {
 
   return (
     <div className={`content-page-sebrae  ${isOpenPage ? 'show' : 'hidden'}`}>
-      <button className='btn-close-page' onClick={() => handleClose()}>
-        <i className='icon-Dribble' />
-      </button>
-      <div className='content-title'>
-        <p className='pre-title'>2018</p>
-        <h1 className='title-page'>Sebrae events <br />e-commerce</h1>
-      </div>
-      <div>
-        <div className='content-card-four'>
-          <div className='content-info'>
-            <div className='d-flex gap-sm'>
-              <Tag name='UI/UX'/>
-              <Tag name='Front end'/>
-            </div>
-            <div className='content-description'>
-              <h3>Optimizing Web Performance for Visual Accessibility</h3>
-              <p>Illustrations Enhancing User Communication: Leveraging <b>SVG</b> Format for Web Optimization without Compromising Performance</p>
-            </div>
-          </div>
-          <Astronaut />
+      <div className='max-grid'>
+        <button className='btn-close-page' onClick={() => handleClose()}>
+          <i className='icon-Dribble' />
+        </button>
+        <div className='content-title'>
+          <p className='pre-title'>2018</p>
+          <h1 className='title-page'>Sebrae events <br />e-commerce</h1>
         </div>
+        <GridCardSebrae />
+        {/* <iframe
+          className='iframe-figma'
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FG5LP5npZCmW2gBvDFcVrec%2FE-commerce-Sebrae%3Ftype%3Ddesign%26node-id%3D1408%253A96%26mode%3Ddesign%26t%3DkgUDVQKdT4kZFEuC-1" allowFullScreen /> */}
       </div>
-      <iframe
-        className='iframe-figma'
-        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FG5LP5npZCmW2gBvDFcVrec%2FE-commerce-Sebrae%3Ftype%3Ddesign%26node-id%3D1408%253A96%26mode%3Ddesign%26t%3DkgUDVQKdT4kZFEuC-1" allowFullScreen />
     </div>
   )
 }
