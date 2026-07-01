68c1d716403a782716797a0589d09879e32108e5
Ramon Mota <ramon_mota@live.com>
2026-07-01 15:18:40 -0300

    add redirect

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const portalRoot = document.getElementById('root')!;
-
-const root = createRoot(portalRoot);
-root.render(<App />)
